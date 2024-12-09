935d5edbd821324a603c7c1568c1629a41c3cd31
Nopnapat Norasri <nopnapatn@gmail.com>
2024-12-10 01:08:41 +0700

    fix(*): run lint fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next"
-
-import { ThemeProvider } from "@/components/theme-provider"
-import "@/styles/globals.css"
 import localFont from "next/font/local"
 
+import { ThemeProvider } from "@/components/theme-provider"
+
+import "@/styles/globals.css"
+
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900"
 })
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900"
 })
