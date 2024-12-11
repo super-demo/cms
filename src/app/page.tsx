4ab20212064f791aa2ba1ad5127d5bfbea9f8a30
Nopnapat Norasri <nopnapatn@gmail.com>
2024-12-12 03:05:49 +0700

    feat(layout): make container for wrap up layout page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,19 @@
 
 import { signOut } from "next-auth/react"
 
+import ContainerLayout from "@/components/layout/container"
+
 export default function Home() {
   return (
-    <div>
+    <ContainerLayout>
       <h1>Super Office | CMS</h1>
       <p>A CMS for Super Office</p>
       <a href="/organization/template/dashboard">
         <p>Go to Dashboard</p>
       </a>
-      <button onClick={() => signOut({ callbackUrl: "/sign-in" })}>
+      <button onClick={() => signOut({ callbackUrl: "/sign" })}>
         <p>Logout</p>
       </button>
-    </div>
+    </ContainerLayout>
   )
 }
