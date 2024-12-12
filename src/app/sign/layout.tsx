268a57a7718ab9fb20cbb51a59dc45e72ab6a81c
Nopnapat Norasri <nopnapatn@gmail.com>
2024-12-12 18:27:58 +0700

    fix(*): run lint fix

--- src/app/sign/layout.tsx
+++ src/app/sign/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode, Suspense } from "react"
 export default async function RootLayout({
   children
 }: Readonly<{
-  children: ReactNode;
+  children: ReactNode
 }>) {
   return <Suspense>{children}</Suspense>
 }
